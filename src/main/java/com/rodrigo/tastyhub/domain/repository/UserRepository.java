839c6552f35f33b1d9fe0a942d9e7f9fe4f6fb08
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2026-02-05 23:25:57 -0300

    :construction: feat: create User Repository

---
+++ src/main/java/com/rodrigo/tastyhub/domain/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.rodrigo.tastyhub.domain.repository;
+
+import com.rodrigo.tastyhub.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+}
