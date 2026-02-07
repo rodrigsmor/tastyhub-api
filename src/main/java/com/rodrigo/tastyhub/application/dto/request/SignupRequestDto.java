297f365887a7fc841d247dea2d1fbcbb17585a87
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2026-02-07 09:52:48 -0300

    :recycle: refactor: update SignupRequestDto to actual flow

--- src/main/java/com/rodrigo/tastyhub/application/dto/request/SignupRequestDto.java
+++ src/main/java/com/rodrigo/tastyhub/application/dto/request/SignupRequestDto.java
@@ -1,9 +1,7 @@
 package com.rodrigo.tastyhub.application.dto.request;
 
-import com.rodrigo.tastyhub.domain.model.UserRole;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
 import java.util.Set;
@@ -22,20 +20,7 @@ public record SignupRequestDto(
     @Size(max = 92, message = "Email must not exceed 92 characters")
     String email,
 
-    @NotBlank(message = "Username is required")
-    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
-    String username,
-
-    @Size(max = 50, message = "Phone must not exceed 50 characters")
-    String phone,
-
-    @Size(max = 280, message = "Bio must not exceed 280 characters")
-    String bio,
-
     @NotBlank(message = "Password is required")
     @Size(min = 8, message = "Password must be at least 8 characters long")
-    String password,
-
-    @NotEmpty(message = "At least one role must be assigned")
-    Set<UserRole> roles
+    String password
 ) {}
