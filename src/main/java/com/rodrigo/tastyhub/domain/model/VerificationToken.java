9838ea65fd2aae5b6b844dc5b344cdf108f00b2c
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2026-02-07 12:10:12 -0300

    :sparkles: feat: create Verification and refresh token entities

---
+++ src/main/java/com/rodrigo/tastyhub/domain/model/VerificationToken.java
@@ -0,0 +1,33 @@
+package com.rodrigo.tastyhub.domain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "verification_tokens")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class VerificationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime expiryDate;
+
+    public boolean isExpired() {
+        return LocalDateTime.now().isAfter(this.expiryDate);
+    }
+}
