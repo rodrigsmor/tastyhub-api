3b6e3f11515407ac72bb9f5e872572246f9f0cf7
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2026-02-20 22:09:39 -0300

    :construction: feat: updates tag and user repositories

--- src/main/java/com/rodrigo/tastyhub/domain/repository/UserRepository.java
+++ src/main/java/com/rodrigo/tastyhub/domain/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.rodrigo.tastyhub.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +13,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Boolean existsByUsername(String username);
     Optional<User> findByUsername(String username);
+    Boolean existsByIdIn(Collection<Long> id);
 }
