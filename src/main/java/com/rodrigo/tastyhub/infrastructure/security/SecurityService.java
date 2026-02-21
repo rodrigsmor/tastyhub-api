8157312f60844c17d8fe1b722675f4171baf9d2b
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2026-02-20 23:57:08 -0300

    :bug: fix: returns the User in the Security Service

--- src/main/java/com/rodrigo/tastyhub/infrastructure/security/SecurityService.java
+++ src/main/java/com/rodrigo/tastyhub/infrastructure/security/SecurityService.java
@@ -1,13 +1,20 @@
 package com.rodrigo.tastyhub.infrastructure.security;
 
 import com.rodrigo.tastyhub.domain.model.User;
+import com.rodrigo.tastyhub.domain.repository.UserRepository;
+import com.rodrigo.tastyhub.exceptions.ResourceNotFoundException;
 import com.rodrigo.tastyhub.exceptions.UnauthorizedException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SecurityService {
+    @Autowired
+    private UserRepository userRepository;
+
     public User getCurrentUser() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
@@ -15,6 +22,9 @@ public class SecurityService {
             throw new UnauthorizedException("User not authenticated");
         }
 
-        return (User) auth.getPrincipal();
+        UserDetails userDetails = (UserDetails) auth.getPrincipal();
+
+        return userRepository.findByEmail(userDetails.getUsername())
+            .orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 }
