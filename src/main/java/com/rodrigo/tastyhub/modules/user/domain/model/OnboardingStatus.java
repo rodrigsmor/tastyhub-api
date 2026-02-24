4e8b08d2582739002b1a840d900855ea79cb73b6
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2026-02-24 20:28:24 -0300

    :recycle: refactor: restructures OnboardingStatus enum

--- src/main/java/com/rodrigo/tastyhub/modules/user/domain/model/OnboardingStatus.java
+++ src/main/java/com/rodrigo/tastyhub/modules/user/domain/model/OnboardingStatus.java
@@ -1,21 +1,11 @@
 package com.rodrigo.tastyhub.modules.user.domain.model;
 
 public enum OnboardingStatus {
-    PENDING_VERIFICATION("User needs to verify email"),
-    STEP_1("First step of onboarding: Profile details"),
-    STEP_2("Second step of onboarding: Preferences"),
-    STEP_3("Third step of onboarding: Final setup"),
-    COMPLETED("Onboarding finished");
-
-    private final String description;
-
-    OnboardingStatus(String description) {
-        this.description = description;
-    }
-
-    public String getDescription() {
-        return description;
-    }
+    PENDING_VERIFICATION,
+    STEP_1,
+    STEP_2,
+    STEP_3,
+    COMPLETED;
 
     public boolean isInProgress() {
         return this != PENDING_VERIFICATION && this != COMPLETED;
