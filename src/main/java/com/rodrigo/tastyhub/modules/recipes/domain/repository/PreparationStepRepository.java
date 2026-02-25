6506066287bae7aad1e7b8b18ddedb9c05236fce
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2026-02-25 13:02:51 -0300

    :construction: feat: creates ingredients repositories

---
+++ src/main/java/com/rodrigo/tastyhub/modules/recipes/domain/repository/PreparationStepRepository.java
@@ -0,0 +1,9 @@
+package com.rodrigo.tastyhub.modules.recipes.domain.repository;
+
+import com.rodrigo.tastyhub.modules.recipes.domain.model.PreparationStep;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PreparationStepRepository extends JpaRepository<PreparationStep, Long> {
+}
