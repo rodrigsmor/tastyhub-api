183c6147fdaaec24e0a7d7c3fea04b67159f85b1
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2026-02-28 22:42:08 -0300

    :bug: fix: adds Builder.default to avoid lombok errors with builder

--- src/main/java/com/rodrigo/tastyhub/modules/recipes/domain/model/RecipeStatistics.java
+++ src/main/java/com/rodrigo/tastyhub/modules/recipes/domain/model/RecipeStatistics.java
@@ -19,15 +19,19 @@ public class RecipeStatistics {
     @JoinColumn(name = "recipe_id")
     private Recipe recipe;
 
+    @Builder.Default
     @Column(name = "favorites_count", nullable = false)
     private Integer favoritesCount = 0;
 
+    @Builder.Default
     @Column(name = "reviews_count", nullable = false)
     private Integer reviewsCount = 0;
 
+    @Builder.Default
     @Column(name = "total_rating_sum", nullable = false)
     private Integer totalRatingSum = 0;
 
+    @Builder.Default
     @Column(name = "average_rating", nullable = false)
     private Double averageRating = 0.0;
 }
