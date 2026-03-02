ca77f5c89ddbef36dcc9d15c477f6b3b49fcc6d0
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2026-03-01 21:27:04 -0300

    :construction: feat: implements the filtered listing of recipes

--- src/main/java/com/rodrigo/tastyhub/modules/recipes/domain/service/RecipeService.java
+++ src/main/java/com/rodrigo/tastyhub/modules/recipes/domain/service/RecipeService.java
@@ -1,24 +1,29 @@
 package com.rodrigo.tastyhub.modules.recipes.domain.service;
 
 import com.rodrigo.tastyhub.modules.recipes.application.dto.request.CreateRecipeDto;
-import com.rodrigo.tastyhub.modules.recipes.application.dto.request.ListRecipesRequest;
+import com.rodrigo.tastyhub.modules.recipes.application.dto.request.ListRecipesQuery;
 import com.rodrigo.tastyhub.modules.recipes.application.dto.response.FullRecipeDto;
 import com.rodrigo.tastyhub.modules.recipes.application.dto.response.RecipePagination;
 import com.rodrigo.tastyhub.modules.recipes.application.dto.response.SummaryRecipeDto;
 import com.rodrigo.tastyhub.modules.recipes.application.mapper.RecipeMapper;
 import com.rodrigo.tastyhub.modules.recipes.domain.model.*;
-import com.rodrigo.tastyhub.modules.recipes.domain.repository.CurrencyRepository;
 import com.rodrigo.tastyhub.modules.recipes.domain.repository.RecipeRepository;
+import com.rodrigo.tastyhub.modules.recipes.infrastructure.persistence.RecipeSpecification;
 import com.rodrigo.tastyhub.modules.tags.domain.model.Tag;
 import com.rodrigo.tastyhub.modules.tags.domain.service.TagService;
 import com.rodrigo.tastyhub.modules.user.domain.annotations.RequiresVerification;
 import com.rodrigo.tastyhub.modules.user.domain.model.User;
 import com.rodrigo.tastyhub.shared.config.security.SecurityService;
 import com.rodrigo.tastyhub.shared.dto.response.PaginationMetadata;
+import com.rodrigo.tastyhub.shared.enums.SortDirection;
 import com.rodrigo.tastyhub.shared.exception.ResourceNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.BadRequestException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
@@ -94,23 +99,49 @@ public class RecipeService {
     }
 
     @Transactional
-    public RecipePagination listRecipes(ListRecipesRequest request) {
-        List<SummaryRecipeDto> recipes = recipeRepository.findAll()
+    public RecipePagination listRecipes(ListRecipesQuery request) {
+        Pageable pageable = PageRequest.of(
+            request.page(),
+            request.size(),
+            buildSort(request.sortBy(), request.direction())
+        );
+
+        Page<Recipe> page = recipeRepository.findAll(
+            RecipeSpecification.withFilters(request),
+            pageable
+        );
+
+        System.out.println(page);
+
+        List<SummaryRecipeDto> recipes = page.getContent()
             .stream()
             .map(RecipeMapper::toSummaryDto)
             .toList();
 
         PaginationMetadata metadata = new PaginationMetadata(
-            request.page(),
-            request.size(),
-            0,
-            0,
+            page.getNumber(),
+            page.getSize(),
+            page.getTotalPages(),
+            page.getTotalElements(),
             request.direction(),
-            true,
-            false
+            page.hasNext(),
+            page.hasPrevious()
         );
 
         return new RecipePagination(recipes, metadata);
+    }
+
+    private Sort buildSort(RecipeSortBy sortBy, SortDirection direction) {
+        String field = switch (sortBy) {
+            case TITLE -> "title";
+            case REVIEWS -> "statistics.averageRating";
+            case RELEVANCE -> "statistics.favoritesCount";
+            default -> "createdAt";
+        };
+
+        return direction == SortDirection.ASC
+            ? Sort.by(field).ascending()
+            : Sort.by(field).descending();
     }
 
     // CREATE, READ, UPDATE, DELETE
