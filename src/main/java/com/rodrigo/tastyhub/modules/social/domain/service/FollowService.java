f35335b885197a46020226252e166e4210a93f73
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2026-03-17 22:29:39 -0300

    :sparkles: feat: creates service for unfollowing user

--- src/main/java/com/rodrigo/tastyhub/modules/social/domain/service/FollowService.java
+++ src/main/java/com/rodrigo/tastyhub/modules/social/domain/service/FollowService.java
@@ -5,6 +5,7 @@ import com.rodrigo.tastyhub.modules.social.domain.model.FollowId;
 import com.rodrigo.tastyhub.modules.social.domain.repository.FollowRepository;
 import com.rodrigo.tastyhub.modules.user.domain.model.User;
 import com.rodrigo.tastyhub.shared.exception.DomainException;
+import com.rodrigo.tastyhub.shared.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -38,5 +39,15 @@ public class FollowService {
         follow.setId(followId);
 
         followRepository.save(follow);
+    }
+
+    public void unfollow(User follower, User following) {
+        FollowId followId = new FollowId(follower.getId(), following.getId());
+
+        if (!followRepository.existsById(followId)) {
+            throw new ResourceNotFoundException("You are not following this user");
+        }
+
+        followRepository.deleteById(followId);
     }
 }
