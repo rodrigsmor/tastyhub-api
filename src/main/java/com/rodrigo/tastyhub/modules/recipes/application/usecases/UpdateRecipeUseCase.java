ba2cc094977398ace17f134263a84ad3217830f0
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2026-03-19 21:08:43 -0300

    :sparkles: feat: creates update recipe use case

--- src/main/java/com/rodrigo/tastyhub/modules/recipes/application/usecases/UpdateRecipeUseCase.java
+++ src/main/java/com/rodrigo/tastyhub/modules/recipes/application/usecases/UpdateRecipeUseCase.java
@@ -3,22 +3,15 @@ package com.rodrigo.tastyhub.modules.recipes.application.usecases;
 import com.rodrigo.tastyhub.modules.recipes.application.dto.request.UpdateRecipeDto;
 import com.rodrigo.tastyhub.modules.recipes.application.dto.response.FullRecipeDto;
 import com.rodrigo.tastyhub.modules.recipes.application.mapper.RecipeMapper;
-import com.rodrigo.tastyhub.modules.recipes.domain.model.Currency;
 import com.rodrigo.tastyhub.modules.recipes.domain.model.Recipe;
 import com.rodrigo.tastyhub.modules.recipes.domain.service.CurrencyService;
 import com.rodrigo.tastyhub.modules.recipes.domain.service.RecipeService;
-import com.rodrigo.tastyhub.modules.tags.domain.model.Tag;
 import com.rodrigo.tastyhub.modules.tags.domain.service.TagService;
 import com.rodrigo.tastyhub.modules.user.domain.annotations.RequiresVerification;
 import com.rodrigo.tastyhub.shared.config.security.SecurityService;
-import com.rodrigo.tastyhub.shared.exception.ForbiddenException;
 import jakarta.transaction.Transactional;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -32,32 +25,27 @@ public class UpdateRecipeUseCase {
     @Transactional
     public FullRecipeDto execute(Long recipeId, UpdateRecipeDto newData) {
         Long userId = securityService.getCurrentUser().getId();
-
         Recipe recipe = recipeService.findByIdOrThrow(recipeId);
 
-        if (!recipe.getAuthor().getId().equals(userId)) {
-            throw new ForbiddenException("You do not have permission to update this recipe");
-        }
+       recipe.validateOwnership(userId);
 
-        Optional.ofNullable(newData.title()).ifPresent(recipe::setTitle);
-        Optional.ofNullable(newData.description()).ifPresent(recipe::setDescription);
+       recipe.update(
+           newData.title(),
+           newData.description(),
+           newData.category(),
+           newData.cookTimeMin(),
+           newData.cookTimeMax(),
+           newData.estimatedCost(),
+           newData.currencyId() != null ? currencyService.findById(newData.currencyId()) : null,
+           newData.tagIds() != null ? tagService.syncAll(newData.tagIds()) : null
+       );
 
-        recipe.updateTiming(recipe.getCookTimeMin(), recipe.getCookTimeMax());
-
-        Currency currency = null;
-
-        if (newData.currencyId() != null) {
-            currency = currencyService.findById(newData.currencyId());
-        }
-
-        recipe.updateMonetaryDetails(newData.estimatedCost(), currency);
-
-        Optional.ofNullable(newData.tagIds())
-            .ifPresent(ids -> {
-                List<Tag> tags = tagService.syncAll(ids);
-                recipe.updateAllTags(tags);
-            });
-
-        return RecipeMapper.toFullRecipeDto(recipeService.update(recipe, newData.ingredients(), newData.steps()));
+        return RecipeMapper.toFullRecipeDto(
+            recipeService.updateAndSync(
+                recipe,
+                newData.ingredients(),
+                newData.steps()
+            )
+        );
     }
 }
