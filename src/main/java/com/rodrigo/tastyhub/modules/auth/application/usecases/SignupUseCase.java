79df7d3a9271e48afeab52721cd25a705717a01c
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2026-03-22 21:06:01 -0300

    :recycle: refactor: implements UseCase with AuthService method

--- src/main/java/com/rodrigo/tastyhub/modules/auth/application/usecases/SignupUseCase.java
+++ src/main/java/com/rodrigo/tastyhub/modules/auth/application/usecases/SignupUseCase.java
@@ -3,19 +3,22 @@ package com.rodrigo.tastyhub.modules.auth.application.usecases;
 import com.rodrigo.tastyhub.modules.auth.application.dto.request.SignupRequestDto;
 import com.rodrigo.tastyhub.modules.auth.application.dto.response.SignupResponseDto;
 import com.rodrigo.tastyhub.modules.auth.domain.service.AuthService;
+import com.rodrigo.tastyhub.modules.user.domain.model.Role;
 import com.rodrigo.tastyhub.modules.user.domain.model.User;
-import com.rodrigo.tastyhub.modules.user.domain.service.UserService;
+import com.rodrigo.tastyhub.modules.user.domain.service.RoleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SignupUseCase {
-    private final UserService userService;
     private final AuthService authService;
+    private final RoleService roleService;
 
     public SignupResponseDto execute(SignupRequestDto request) {
-        User user = this.userService.createNewUser(request);
+        Role role = roleService.getDefaultRole();
+
+        User user = this.authService.signup(request, role);
 
         String verificationToken = this.authService.createVerificationToken(user);
 
