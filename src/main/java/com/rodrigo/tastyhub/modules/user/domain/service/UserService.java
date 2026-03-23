76d8d3c5fd54bdd092514413a00a2d96601a4fca
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2026-03-22 21:03:13 -0300

    :recycle: refactor: moves create New User to Auth Service

--- src/main/java/com/rodrigo/tastyhub/modules/user/domain/service/UserService.java
+++ src/main/java/com/rodrigo/tastyhub/modules/user/domain/service/UserService.java
@@ -1,6 +1,5 @@
 package com.rodrigo.tastyhub.modules.user.domain.service;
 
-import com.rodrigo.tastyhub.modules.auth.application.dto.request.SignupRequestDto;
 import com.rodrigo.tastyhub.modules.user.application.dto.response.UserSummaryDto;
 import com.rodrigo.tastyhub.modules.user.application.mapper.UserMapper;
 import com.rodrigo.tastyhub.modules.user.domain.model.User;
@@ -8,30 +7,21 @@ import com.rodrigo.tastyhub.modules.user.domain.projections.UserProfileProjectio
 import com.rodrigo.tastyhub.modules.user.domain.repository.UserRepository;
 import com.rodrigo.tastyhub.shared.config.security.SecurityService;
 import com.rodrigo.tastyhub.shared.config.storage.ImageStorageService;
-import com.rodrigo.tastyhub.shared.exception.DomainException;
 import com.rodrigo.tastyhub.shared.exception.ResourceNotFoundException;
 import com.rodrigo.tastyhub.shared.kernel.annotations.FileCleanup;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.Nullable;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
 
-    private final PasswordEncoder passwordEncoder;
-
     private final ImageStorageService imageStorageService;
 
     private final SecurityService securityService;
-
-    private final RoleService roleService;
 
     public boolean existsById(Long userId) {
         return userRepository.existsById(userId);
@@ -45,28 +35,6 @@ public class UserService {
     public User findByIdOrThrow(Long userId) {
         return userRepository.findById(userId)
             .orElseThrow(() -> new ResourceNotFoundException("The user provided cannot be found"));
-    }
-
-    @Transactional
-    public User createNewUser(SignupRequestDto dto) {
-        if (userRepository.existsByEmail(dto.email())) {
-            throw new DomainException("This email is already in use!");
-        }
-
-        User user = new User();
-        user.setFirstName(dto.firstName());
-        user.setLastName(dto.lastName());
-        user.setEmail(dto.email());
-        user.setUsername(dto.email());
-
-        user.setupPassword(dto.password(), passwordEncoder);
-
-        user.setRoles(Set.of(roleService.getDefaultRole()));
-        user.createDefaultCollections();
-
-        user.initializeSettings();
-
-        return userRepository.save(user);
     }
 
     @FileCleanup
