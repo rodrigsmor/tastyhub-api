c4e17128b9aa0d241a8ef2c41bf089552307b4b9
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2026-03-25 11:46:48 -0300

    :sparkles: feat: implements JpaSpecificationExecutor in article repository

--- src/main/java/com/rodrigo/tastyhub/modules/articles/domain/repository/ArticleRepository.java
+++ src/main/java/com/rodrigo/tastyhub/modules/articles/domain/repository/ArticleRepository.java
@@ -2,9 +2,10 @@ package com.rodrigo.tastyhub.modules.articles.domain.repository;
 
 import com.rodrigo.tastyhub.modules.articles.domain.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends JpaRepository<Article, Long>, JpaSpecificationExecutor<Article> {
     long countByUserId(Long userId);
 }
