7c4482f6cd966292962b336a7e4480fab9c2329b
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2026-03-24 22:35:53 -0300

    :recycle: refactor: implements new method to preparer all recipes

--- src/main/java/com/rodrigo/tastyhub/modules/recipes/domain/service/IngredientService.java
+++ src/main/java/com/rodrigo/tastyhub/modules/recipes/domain/service/IngredientService.java
@@ -1,12 +1,18 @@
 package com.rodrigo.tastyhub.modules.recipes.domain.service;
 
 import com.rodrigo.tastyhub.modules.recipes.domain.model.Ingredient;
+import com.rodrigo.tastyhub.modules.recipes.domain.model.Recipe;
+import com.rodrigo.tastyhub.modules.recipes.domain.model.RecipeIngredient;
+import com.rodrigo.tastyhub.modules.recipes.domain.model.RecipeIngredientCommand;
 import com.rodrigo.tastyhub.modules.recipes.domain.repository.IngredientRepository;
 import com.rodrigo.tastyhub.shared.exception.DomainException;
+import com.rodrigo.tastyhub.shared.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -20,5 +26,32 @@ public class IngredientService {
     public Ingredient findByIdOrThrow(Long id) {
         return this.ingredientRepository.findById(id)
             .orElseThrow(() -> new DomainException("Ingredient couldn't be found!"));
+    }
+
+    public List<RecipeIngredient> preparerAll(List<RecipeIngredientCommand> recipeIngredients, Recipe recipe) {
+        if (recipeIngredients == null || recipeIngredients.isEmpty()) {
+            throw new IllegalArgumentException("It must have at least one ingredient!");
+        }
+
+        return recipeIngredients.stream().map(recipeIngredient -> {
+            if (recipeIngredient.id() != null) {
+                return RecipeIngredient.builder()
+                    .id(recipeIngredient.id())
+                    .quantity(recipeIngredient.quantity())
+                    .unit(recipeIngredient.unit())
+                    .recipe(recipe)
+                    .build();
+            } else {
+                Ingredient ingredient = this.findById(recipeIngredient.ingredientId())
+                    .orElseThrow(() -> new ResourceNotFoundException("Ingredient not found"));
+
+                return RecipeIngredient.builder()
+                    .ingredient(ingredient)
+                    .quantity(recipeIngredient.quantity())
+                    .unit(recipeIngredient.unit())
+                    .recipe(recipe)
+                    .build();
+            }
+        }).collect(Collectors.toList());
     }
 }
