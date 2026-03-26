db183d8149d67b2fd5f971aaf84296041ec277d4
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2026-03-26 10:14:19 -0300

    :label: feat: creates DTO to articles creation

---
+++ src/main/java/com/rodrigo/tastyhub/modules/articles/application/dto/request/CreateArticleDto.java
@@ -0,0 +1,28 @@
+package com.rodrigo.tastyhub.modules.articles.application.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Schema(description = "Payload to create a new article")
+public record CreateArticleDto(
+    @Schema(description = "Title of the Article", example = "10 Essential Spices Every Home Cook Should Own")
+    @NotBlank(message = "Title is required")
+    @Size(min = 3, max = 100)
+    String title,
+
+    @Schema(
+        description = "The content of the Article",
+        example = "Cooking is a big challenge, but there are some best practices that can help you achieve better results. A tasty, appealing, and mouth-watering meal requires preparation, patience, and practice. Check out my thoughts below:"
+    )
+    @NotNull(message = "Article must have a content!")
+    String content,
+
+    @Schema(example = "true", description = "Visibility of recipe")
+    Boolean isPublic,
+
+    @Schema(description = "ISO 639-1 language code with region", example = "en-US")
+    @NotBlank(message = "Language is required")
+    String language
+) {}
