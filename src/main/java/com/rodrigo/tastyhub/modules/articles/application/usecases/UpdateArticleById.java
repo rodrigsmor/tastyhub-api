b7f67841524876dd654d2838672833efc291c774
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2026-03-27 21:25:03 -0300

    :sparkles: feat: creates UpdateArticleById use case

---
+++ src/main/java/com/rodrigo/tastyhub/modules/articles/application/usecases/UpdateArticleById.java
@@ -0,0 +1,33 @@
+package com.rodrigo.tastyhub.modules.articles.application.usecases;
+
+import com.rodrigo.tastyhub.modules.articles.application.dto.request.UpdateArticleDto;
+import com.rodrigo.tastyhub.modules.articles.application.dto.response.FullArticleDto;
+import com.rodrigo.tastyhub.modules.articles.application.mapper.ArticleMapper;
+import com.rodrigo.tastyhub.modules.articles.domain.model.Article;
+import com.rodrigo.tastyhub.modules.articles.domain.service.ArticleService;
+import com.rodrigo.tastyhub.modules.user.domain.model.User;
+import com.rodrigo.tastyhub.shared.config.security.SecurityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UpdateArticleById {
+    private final ArticleService articleService;
+    private final SecurityService securityService;
+
+    public FullArticleDto execute(Long articleId, UpdateArticleDto newData) {
+        User user = this.securityService.getCurrentUser();
+
+        Article article = articleService.update(
+            articleId,
+            newData.title(),
+            newData.content(),
+            newData.isPublic(),
+            newData.language(),
+            user.getId()
+        );
+
+        return ArticleMapper.toFull(article);
+    }
+}
