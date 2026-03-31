d814eac416b890870c54c79e97656af19c05701e
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2026-03-31 19:51:37 -0300

    :books: docs: documents the enum of onboarding statuses

--- src/main/java/com/rodrigo/tastyhub/modules/user/domain/model/OnboardingStatus.java
+++ src/main/java/com/rodrigo/tastyhub/modules/user/domain/model/OnboardingStatus.java
@@ -1,10 +1,22 @@
 package com.rodrigo.tastyhub.modules.user.domain.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Represents the possible states of a user during the onboarding process")
 public enum OnboardingStatus {
+    @Schema(description = "User has registered but hasn't verified their email yet")
     PENDING_VERIFICATION,
+
+    @Schema(description = "Basic profile information (username, bio) needs to be filled")
     STEP_1,
+
+    @Schema(description = "Preferences or additional details (categories, interests) pending")
     STEP_2,
+
+    @Schema(description = "Final configuration or tutorial completion")
     STEP_3,
+
+    @Schema(description = "Onboarding process finished successfully")
     COMPLETED;
 
     public boolean isInProgress() {
