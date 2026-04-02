49f0c6898672d35b3c20f8aa69998d6fcd391de9
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2026-04-01 22:31:37 -0300

    :test_tube: test: creates Article Service tests class

---
+++ src/test/java/com/rodrigo/tastyhub/modules/articles/domain/service/ArticleServiceTest.java
@@ -0,0 +1,10 @@
+package com.rodrigo.tastyhub.modules.articles.domain.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ArticleServiceTest {
+
+}
